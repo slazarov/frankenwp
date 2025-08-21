9ef364937f92b858b2127b5ac7c8c30117456f05
slazarov <s.lazarov@lp-sofia.com>
2025-08-21 21:01:59 +0300

    Remove passwrd

--- plugins-install.sh
+++ plugins-install.sh
@@ -1,9 +1,11 @@
 # Install WordPress if not yet installed
 if ! wp core is-installed --path=/var/www/html --allow-root >/dev/null 2>&1; then
     echo "Installing WordPress..."
-    wp core install --url="$WP_URL" --title="$WP_TITLE" \
-        --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASS" \
-        --admin_email="$WP_ADMIN_EMAIL" --path=/var/www/html --allow-root
+    wp core install --url="$WP_URL" \
+    	--title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN"  \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path=/var/www/html --allow-root
 fi
 
 # Install plugins in background
