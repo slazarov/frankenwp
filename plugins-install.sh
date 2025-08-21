cba4aa3c429ed63c8c7bcff43311976d8e9fe79f
slazarov <s.lazarov@lp-sofia.com>
2025-08-21 20:44:11 +0300

    Wait for installed wp

--- plugins-install.sh
+++ plugins-install.sh
@@ -1,10 +1,17 @@
+# Wait until WP is installed
+until wp core is-installed --path=/var/www/html --allow-root >/dev/null 2>&1; do
+    echo "Waiting for WordPress to be installed..."
+    sleep 5
+done
+
+# Now install plugins
 if [ -n "$PLUGINS" ]; then
-	IFS=',' read -ra PLUGIN_ARRAY <<<"$PLUGINS"
-	for plugin in "${PLUGIN_ARRAY[@]}"; do
-		plugin=$(echo "$plugin" | xargs)
-		if ! wp plugin is-installed "$plugin" --path=/var/www/html --allow-root >/dev/null 2>&1; then
-			echo "Installing plugin: $plugin"
-			wp plugin install "$plugin" --activate --path=/var/www/html --allow-root
-		fi
-	done
+    IFS=',' read -ra PLUGIN_ARRAY <<<"$PLUGINS"
+    for plugin in "${PLUGIN_ARRAY[@]}"; do
+        plugin=$(echo "$plugin" | xargs)
+        if ! wp plugin is-installed "$plugin" --path=/var/www/html --allow-root >/dev/null 2>&1; then
+            echo "Installing plugin: $plugin"
+            wp plugin install "$plugin" --activate --path=/var/www/html --allow-root
+        fi
+    done
 fi
